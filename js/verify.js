c9a4d71f5ed22c7799d06cb4c9199a5d8830b74f
kromate <akpananthony33@gmail.com>
2020-06-01 02:05:20 +0100

    basic

--- js/verify.js
+++ js/verify.js
@@ -3,7 +3,18 @@
 
 document.getElementById('verify').addEventListener('click', function () {
 
-  var user = firebase.auth().currentUser;
+  var user = firebase.auth().currentUser.email;
+
+  if (user != null) {
+    user.providerData.forEach(function (profile) {
+      console.log(profile)
+      console.log("Sign-in provider: " + profile.providerId);
+      console.log("  Provider-specific UID: " + profile.uid);
+      console.log("  Name: " + profile.displayName);
+      console.log("  Email: " + profile.email);
+      console.log("  Photo URL: " + profile.photoURL);
+    });
+  }
 
   user.sendEmailVerification().then(function () {
     // Email sent.
@@ -15,3 +26,4 @@ document.getElementById('verify').addEventListener('click', function () {
 
 
 })
+
